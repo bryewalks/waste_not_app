053d832e7957c51db682543a19efb1d0d487bb06
bryewalks <bryewalks@gmail.com>
2019-02-19 16:40:32 -0600

    working

--- app/controllers/api/fridge_items_controller.rb
+++ app/controllers/api/fridge_items_controller.rb
@@ -1,3 +1,31 @@
 class Api::FridgeItemsController < ApplicationController
-  
+  def index
+    
+  end
+
+  def create
+    @fridge_items = FridgeItem.new(
+                                    name: params[:name],
+                                    price: params[:price],
+                                    purchase_date: params[:purchase_date]
+                                  )
+    if @fridge_items.save
+      render 'show.json.jbuilder'
+    else
+      
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+
+  end
+
 end
